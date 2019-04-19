086931d85b2ee9199ff269561ec02c07758fd620
Turner Barnett <43420540+tmbarn10@users.noreply.github.com>
2019-04-19 16:34:22 -0400

    Add files via upload

---
+++ Prog2/Prog2/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Prog2
+{
+    partial class Prog2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.classStandingGroupBox = new System.Windows.Forms.GroupBox();
+            this.seniorRadioBtn = new System.Windows.Forms.RadioButton();
+            this.juniorRadioBtn = new System.Windows.Forms.RadioButton();
+            this.sophomoreRadioBtn = new System.Windows.Forms.RadioButton();
+            this.freshmanRadioBtn = new System.Windows.Forms.RadioButton();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.lastNameTxt = new System.Windows.Forms.TextBox();
+            this.findButton = new System.Windows.Forms.Button();
+            this.dateTimeLabel = new System.Windows.Forms.Label();
+            this.classStandingGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // classStandingGroupBox
+            // 
+            this.classStandingGroupBox.Controls.Add(this.seniorRadioBtn);
+            this.classStandingGroupBox.Controls.Add(this.juniorRadioBtn);
+            this.classStandingGroupBox.Controls.Add(this.sophomoreRadioBtn);
+            this.classStandingGroupBox.Controls.Add(this.freshmanRadioBtn);
+            this.classStandingGroupBox.Location = new System.Drawing.Point(12, 24);
+            this.classStandingGroupBox.Name = "classStandingGroupBox";
+            this.classStandingGroupBox.Size = new System.Drawing.Size(104, 114);
+            this.classStandingGroupBox.TabIndex = 0;
+            this.classStandingGroupBox.TabStop = false;
+            this.classStandingGroupBox.Text = "Class Standing:";
+            // 
+            // seniorRadioBtn
+            // 
+            this.seniorRadioBtn.AutoSize = true;
+            this.seniorRadioBtn.Location = new System.Drawing.Point(6, 88);
+            this.seniorRadioBtn.Name = "seniorRadioBtn";
+            this.seniorRadioBtn.Size = new System.Drawing.Size(55, 17);
+            this.seniorRadioBtn.TabIndex = 4;
+            this.seniorRadioBtn.TabStop = true;
+            this.seniorRadioBtn.Text = "Senior";
+            this.seniorRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // juniorRadioBtn
+            // 
+            this.juniorRadioBtn.AutoSize = true;
+            this.juniorRadioBtn.Location = new System.Drawing.Point(6, 65);
+            this.juniorRadioBtn.Name = "juniorRadioBtn";
+            this.juniorRadioBtn.Size = new System.Drawing.Size(53, 17);
+            this.juniorRadioBtn.TabIndex = 3;
+            this.juniorRadioBtn.TabStop = true;
+            this.juniorRadioBtn.Text = "Junior";
+            this.juniorRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // sophomoreRadioBtn
+            // 
+            this.sophomoreRadioBtn.AutoSize = true;
+            this.sophomoreRadioBtn.Location = new System.Drawing.Point(6, 42);
+            this.sophomoreRadioBtn.Name = "sophomoreRadioBtn";
+            this.sophomoreRadioBtn.Size = new System.Drawing.Size(79, 17);
+            this.sophomoreRadioBtn.TabIndex = 2;
+            this.sophomoreRadioBtn.TabStop = true;
+            this.sophomoreRadioBtn.Text = "Sophomore";
+            this.sophomoreRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // freshmanRadioBtn
+            // 
+            this.freshmanRadioBtn.AutoSize = true;
+            this.freshmanRadioBtn.Location = new System.Drawing.Point(6, 19);
+            this.freshmanRadioBtn.Name = "freshmanRadioBtn";
+            this.freshmanRadioBtn.Size = new System.Drawing.Size(71, 17);
+            this.freshmanRadioBtn.TabIndex = 1;
+            this.freshmanRadioBtn.TabStop = true;
+            this.freshmanRadioBtn.Text = "Freshman";
+            this.freshmanRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(122, 68);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(61, 13);
+            this.lastNameLabel.TabIndex = 1;
+            this.lastNameLabel.Text = "Last Name:";
+            // 
+            // lastNameTxt
+            // 
+            this.lastNameTxt.Location = new System.Drawing.Point(189, 65);
+            this.lastNameTxt.Name = "lastNameTxt";
+            this.lastNameTxt.Size = new System.Drawing.Size(83, 20);
+            this.lastNameTxt.TabIndex = 2;
+            // 
+            // findButton
+            // 
+            this.findButton.Location = new System.Drawing.Point(73, 153);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(138, 23);
+            this.findButton.TabIndex = 3;
+            this.findButton.Text = "Find Registration Time";
+            this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
+            // 
+            // dateTimeLabel
+            // 
+            this.dateTimeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dateTimeLabel.Location = new System.Drawing.Point(55, 195);
+            this.dateTimeLabel.Name = "dateTimeLabel";
+            this.dateTimeLabel.Size = new System.Drawing.Size(174, 23);
+            this.dateTimeLabel.TabIndex = 4;
+            this.dateTimeLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Prog2
+            // 
+            this.AcceptButton = this.findButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.dateTimeLabel);
+            this.Controls.Add(this.findButton);
+            this.Controls.Add(this.lastNameTxt);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.classStandingGroupBox);
+            this.Name = "Prog2";
+            this.Text = "Prog2";
+            this.classStandingGroupBox.ResumeLayout(false);
+            this.classStandingGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox classStandingGroupBox;
+        private System.Windows.Forms.RadioButton seniorRadioBtn;
+        private System.Windows.Forms.RadioButton juniorRadioBtn;
+        private System.Windows.Forms.RadioButton sophomoreRadioBtn;
+        private System.Windows.Forms.RadioButton freshmanRadioBtn;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox lastNameTxt;
+        private System.Windows.Forms.Button findButton;
+        private System.Windows.Forms.Label dateTimeLabel;
+    }
+}
+
---
+++ Prog2/Prog2/Form1.cs
@@ -0,0 +1,143 @@
+﻿// Program 2
+// CIS 199-02
+// Due: 3/9/2017
+// Grading ID: B3049
+
+// This program uses four radio buttons and a text box for input in order to calculate when a student at the University of Louisville
+// is supposed to sign up for summer and fall classes. The four radio buttons are for each class standing: freshman, sophomore, junior,
+// and senior. The text box is used for typing in your last name. When you press the Find Registration Date button, the program will
+// calculate your registration date based on the first letter of your last name and what you have selected as your class standing.
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog2
+{
+    public partial class Prog2 : Form
+    {
+        public Prog2()
+        {
+            InitializeComponent();
+        }
+
+        private void findButton_Click(object sender, EventArgs e) // click button that finds your registration date based on radio button clicked and input within the last name text box
+        {
+            const string TIME_ONE = "8:30 AM"; // String constants for all of the possible registration times
+            const string TIME_TWO = "10:00 AM";
+            const string TIME_THREE = "11:30 AM";
+            const string TIME_FOUR = "2:00 PM";
+            const string TIME_FIVE = "4:00 PM";
+
+            string lastName; // Variable for last name, which is what is in the last name text box
+            char lastNameFirstLetter; // Character variable for first letter of your last name
+            string dateOutput = "Error"; // Registration date
+            string timeOutput = "Error"; // Registration time
+
+            lastName = lastNameTxt.Text; // Identifying lastName variable as what is in the lasNameTxt textbox
+            
+                lastName = lastName.ToUpper(); // Last name upper case
+                lastNameFirstLetter = lastName[0]; // Identifies lastNameLetter as the first character in lastName
+
+                
+                    if (seniorRadioBtn.Checked) // If the senior radio button is checked
+                    {
+                        dateOutput = "March 29"; // Seniors register on March 29
+
+                        if (lastNameFirstLetter <= 'D') // A-D
+                            timeOutput = TIME_THREE;
+                        else if (lastNameFirstLetter <= 'I') // E-I
+                            timeOutput = TIME_FOUR;
+                        else if (lastNameFirstLetter <= 'O') // J-O
+                            timeOutput = TIME_FIVE;
+                        else if (lastNameFirstLetter <= 'S') // P-S
+                            timeOutput = TIME_ONE;
+                        else
+                            timeOutput = TIME_TWO; // T-Z
+                    }
+                    else if (juniorRadioBtn.Checked) // If junior radio button is checked
+                    {
+                        dateOutput = "March 30"; // Juniors register on March 30
+
+                        if (lastNameFirstLetter <= 'D') // A-D
+                            timeOutput = TIME_THREE;
+                        else if (lastNameFirstLetter <= 'I') // E-I
+                            timeOutput = TIME_FOUR;
+                        else if (lastNameFirstLetter <= 'O') // J-O
+                            timeOutput = TIME_FIVE;
+                        else if (lastNameFirstLetter <= 'S') // P-S
+                            timeOutput = TIME_ONE; 
+                        else
+                            timeOutput = TIME_TWO; // T-Z
+                    }
+                    else if (sophomoreRadioBtn.Checked) // Sophomore radio button is checked
+                    {
+                        if ((lastNameFirstLetter >= 'C') && (lastNameFirstLetter <= 'O')) // C-D, E-F, G-I, J-L, M-O
+                            dateOutput = "April 3";
+                        else
+                            dateOutput = "March 31"; // P-Q, R-S, T-V, W-Z, A-B
+
+                        if (lastNameFirstLetter <= 'B') // A-B
+                            timeOutput = TIME_FIVE;
+                        else if (lastNameFirstLetter <= 'D') // C-D
+                            timeOutput = TIME_ONE;
+                        else if (lastNameFirstLetter <= 'F') // E-F
+                            timeOutput = TIME_TWO;
+                        else if (lastNameFirstLetter <= 'I') // G-I
+                            timeOutput = TIME_THREE;
+                        else if (lastNameFirstLetter <= 'L') // J-L
+                            timeOutput = TIME_FOUR;
+                        else if (lastNameFirstLetter <= 'O') // M-O
+                            timeOutput = TIME_FIVE;
+                        else if (lastNameFirstLetter <= 'Q') // P-Q
+                            timeOutput = TIME_ONE;
+                        else if (lastNameFirstLetter <= 'S') // R-S
+                            timeOutput = TIME_TWO;
+                        else if (lastNameFirstLetter <= 'V') // T-V
+                            timeOutput = TIME_THREE;
+                        else
+                            timeOutput = TIME_FOUR; // W-Z
+                    }
+                    else if (freshmanRadioBtn.Checked) // Freshman radio button is checked
+                    {
+                        if ((lastNameFirstLetter >= 'C') && (lastNameFirstLetter <= 'O')) // C-D, E-F, G-I, J-L, M-O
+                            dateOutput = "April 5";
+                        else
+                            dateOutput = "April 4"; // P-Q, R-S, T-V, W-Z, A-B
+
+                        if (lastNameFirstLetter <= 'B') // A-B
+                            timeOutput = TIME_FIVE; 
+                        else if (lastNameFirstLetter <= 'D') // C-D
+                            timeOutput = TIME_ONE;
+                        else if (lastNameFirstLetter <= 'F') // E-F
+                            timeOutput = TIME_TWO;
+                        else if (lastNameFirstLetter <= 'I') // G-I
+                            timeOutput = TIME_THREE;
+                        else if (lastNameFirstLetter <= 'L') // J-L
+                            timeOutput = TIME_FOUR;
+                        else if (lastNameFirstLetter <= 'O') // M-O
+                            timeOutput = TIME_FIVE;
+                        else if (lastNameFirstLetter <= 'Q') // P-Q
+                            timeOutput = TIME_ONE;
+                        else if (lastNameFirstLetter <= 'S') // R-S
+                            timeOutput = TIME_TWO;
+                        else if (lastNameFirstLetter <= 'V') // T-V
+                            timeOutput = TIME_THREE;
+                        else
+                            timeOutput = TIME_FOUR; // W-Z
+                    }
+
+                    dateTimeLabel.Text = dateOutput + ", " + timeOutput; // Concatenates date ", " and time and puts it in the dateTimeLabel label.
+
+            }
+            
+    }
+}
